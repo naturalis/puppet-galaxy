354354a9bf70277b167286ef29ab43918738a80c
Atze de Vries <atze.devries@naturalis.nl>
2016-12-27 16:38:19 +0100

    added many things

--- manifests/install.pp
+++ manifests/install.pp
@@ -12,6 +12,40 @@ class galaxy::install inherits ::galaxy::params
   file {'/docker-entrypoint.sh':
     ensure => $::galaxy::params::docker_payload,
     source => 'puppet:///modules/galaxy/docker/docker-entrypoint.sh',
-    mode   => '0770',
+  }
+
+  file {'/opt/env_to_ini.py':
+    ensure => $::galaxy::params::docker_payload,
+    source => 'puppet:///modules/galaxy/docker/env_to_ini.py',
+  }
+
+  if ($::galaxy::params::docker_build) {
+
+    file {'/opt/galaxy.ini.template':
+      content => template('galaxy/galaxy.ini.template.erb'),
+    }
+
+    python::dotfile { '/root/.pip/pip.conf':
+      config => {
+        'global' => {
+        'index-url' => 'https://wheels.galaxyproject.org/simple',
+        }
+      }
+    }
+
+    python::requirements { '/opt/galaxy/requirements.txt':
+      require => [ Vcsrepo['/opt/galaxy'], Python::Dotfile['/root/.pip/pip.conf'] ]
+    }
+
+    python::pip { 'psycopg2':
+      ensure  => '2.6.1',
+      require => Python::Dotfile['/root/.pip/pip.conf'],
+    }
+
+  }else{
+    file {'/opt/galaxy/config/galaxy.ini':
+      content => template('galaxy/galaxy.ini.template.erb'),
+      require => Vcsrepo['/opt/galaxy'],
+    }
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -7,14 +7,18 @@ class galaxy::params {
   $docker_build = true
   $db_host = 'localhost'
   $db_pass = 'password'
+  $gmail_user = 'galaxymail'
+  $gmail_password = 'galaxy_password'
+  $gmail_address = 'galaxymail@gmail.com'
+  $admin_users = 'None'
 
   if ($docker_build) {
     $galaxy_db = '__GALAXY_DB'
     $docker_payload = present
+    $pip
   }else{
     $galaxy_db = "postgres://postgres:${db_pass}@${db_host}:5432/postgres"
     $docker_payload = absent
   }
-
 
 }
